36bf365fc27aec7b5e55e7ad0c63190bf075cca8
mmyoji <mmyoji@gmail.com>
2019-05-02 12:06:16 +0900

    Fix test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -17,7 +17,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create user" do
     assert_difference('User.count') do
-      post users_url, params: { user: { username: @user.username } }
+      post users_url, params: { user: { username: @user.username + "tmp" } }
     end
 
     assert_redirected_to user_url(User.last)
